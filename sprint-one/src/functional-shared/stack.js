a4beee64c732209076621ad55d485765abbf8d3d
rahimftd <rahim.dharssi@gmail.com>
2016-03-02 15:52:09 -0800

    Implement functional-shared version of stack data structure

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.items = 0;
+  someInstance.storage = {};
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.items] = value;
+    this.items++;
+  },
+  pop: function() {
+    this.items = Math.max(0, --this.items);
+    var last = this.storage[this.items];
+    delete this.storage[this.items];
+    return last;
+  },
+  size: function() {
+    return this.items;
+  }
+};
 
+var extend = function(to, otherObj) {
+  for (var key in otherObj) {
+    to[key] = otherObj[key];
+  }
+};
 
