f3c88b3413c44ac06ba77ab9b7dc52a0624ee722
rahimftd <rahim.dharssi@gmail.com>
2016-03-03 11:41:34 -0800

    Pass first 3 tests for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,21 +3,26 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
+
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = node;
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.storage[node] === node ? true : false;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.storage[node];
 };
 
 // ------------------------
@@ -39,6 +44,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
