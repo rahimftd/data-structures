56cf2a58e9167678c5c25bfd3d544c5b5c2602ad
rahimftd <rahim.dharssi@gmail.com>
2016-03-02 17:24:49 -0800

    Implement prototypal version of queue data structure

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someQueue = Object.create(queueMethods);
+  someQueue.storage = {};
+  someQueue.items = 0;
+
+  return someQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.items] = value;
+    this.items++;
+  },
+  dequeue: function() {
+    var first = this.storage[0];
+    this.items && this.items--;
+    for (var i = 0; i < Object.keys(this.storage).length; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[this.items];
+    return first;
+  },
+  size: function() {
+    return this.items;
+  }
+};
 
 
