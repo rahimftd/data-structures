21c1062b18a0e1e5143a54ddea6378769599b39b
rahimftd <rahim.dharssi@gmail.com>
2016-03-02 18:12:47 -0800

    Implement pseudoclassical version of stack data structure

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.items = 0;
+
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.items] = value;
+  this.items++;
+};
+
+Stack.prototype.pop = function() {
+  var last = this.storage[this.items - 1];
+  delete this.storage[this.items - 1];
+  this.items && this.items--;
+  return last;
+};
+
+Stack.prototype.size = function() {
+  return this.items;
+};
 
