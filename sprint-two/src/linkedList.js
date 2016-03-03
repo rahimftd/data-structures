d94862ddf381374e84704397551bfb7cb0ed0158
rahimftd <rahim.dharssi@gmail.com>
2016-03-03 10:50:13 -0800

    Implement list data structure

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.tail !== null) {
+      list.tail.next = node;
+    } 
+    list.tail = node;
+    if (list.head === null) {
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    var originalHead = list.head;
+    list.head = list.head.next;
+    return originalHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (target === currentNode.value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +45,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail O(1)
+ * removeHead O(1)
+ * contains O(n)
  */
