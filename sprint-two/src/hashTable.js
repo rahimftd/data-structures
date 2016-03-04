8a05c8d63d555666d73bf49eee60f840a02ac8bf
rahimftd <rahim.dharssi@gmail.com>
2016-03-03 20:17:40 -0800

    Implement hash table data structure

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,38 +1,94 @@
 
-
+// Creates a constructor for HashTable (pseudoclassical)
 var HashTable = function() {
+  // Limit denotes the maximum size of the table
   this._limit = 8;
+  // Instantiate a 'Limited Array' that will store hash table data
   this._storage = LimitedArray(this._limit);
 };
-
+// Insert method that HashTable instances will delegate to
 HashTable.prototype.insert = function(k, v) {
+  // Creating an index based on a hash function
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Initializing a pair object that stores the key and 
+  // value that is provided
+  var pair = {
+    key: k,
+    value: v
+  };
+  // Checks whether the hash table has a value at the index.
+  // If there is already a value, that means there is a 'collision'
   if (this._storage.get(index) !== undefined) {
+    // Creates a hashArray that is going to store pairs
     var hashArray = [];
-    hashArray = hashArray.concat(this._storage.get(index), v);
-    this._storage.set(index, hashArray);  
-  } else {
-    this._storage.set(index, v);
-  }
-};
-
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var ind;
-  if (Array.isArray(this._storage.get(index))) {
-    if (this._storage.get(index).indexOf(k) !== -1) {
-      return k;
+    // Add the existing value(s) to the hashArray
+    hashArray = hashArray.concat(this._storage.get(index));
+    // Iterate through the hashArray
+    for (var i = 0; i < hashArray.length; i++) {
+      // Check if key (provided by user) is equal to any key
+      // already in hashArray
+      if (k === hashArray[i].key) {
+        // Replace the existing value at the existing key with
+        // the new (user provided) value
+        hashArray[i].value = v;
+        break;
+      // Checks if we have reached the end of the hashArray
+      } else if (i === hashArray.length - 1) { 
+        // Add the new pair to the hashArray
+        // This will only happen if a matching key 
+        // has not already been found in the hashArray
+        hashArray = hashArray.concat(pair);
+      }
     }
-  } else {
-    return this._storage.get(index);
+    this._storage.set(index, hashArray);  
+  } else { // If there is no collision it will simply store the pair
+    this._storage.set(index, pair);
   }
 };
-
+// Function that HashTable instances delegate to. Returns a value based on a provided key
+HashTable.prototype.retrieve = function(k) {
+  // Calculates an index based on a hash function and the provided key
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  // Variables that temporarily stores the pair(s) at index
+  var entries = this._storage.get(index);
+  // Checks if entries is an array
+  if (Array.isArray(entries)) {
+    // Iterate through the entries array
+    for (var i = 0; i < entries.length; i++) {
+      // Check if the key at index i is equal to the user provided key
+      if (k === entries[i].key) {
+        // Returns the value at index i
+        return entries[i].value;
+      }
+    }
+  // Checks if entries exists
+  } else if (entries) {
+    // Returns the value (which corresponds to the user provided key)
+    return entries.value;
+  }
+};
+// Function that HashTable instances delegate to. Removes a value at an index calculated from the given key
 HashTable.prototype.remove = function(k) {
+  // Calculates an index based on a hash function and the provided key
   var removeIndex = getIndexBelowMaxForKey(k, this._limit);
+  // Iterates through the hash table's storage
   this._storage.each(function(value, index, storage) {
-    if (index === removeIndex) {
-      delete storage[index];
+    // Checks if the value is an array (which means that there are multiple entries at the index)
+    if (Array.isArray(value)) {
+      // Iterates through the array of entries
+      for (var i = 0; i < value.length; i++) {
+        // If an entry's key is equal to k (the user provided key)
+        if (value[i].key === k) {
+          // Deletes the entry
+          delete value[i];
+        }
+      }
+    } else {
+      // Checks if the current index in the iteration is equal to the calculated removeIndex
+      if (index === removeIndex) {
+        // Deletes the entry at index
+        delete storage[index];
+      }
     }
   });
 };
