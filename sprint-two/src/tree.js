20c56b909d8f9d6897a1962274d81792cff6170e
rahimftd <rahim.dharssi@gmail.com>
2016-03-05 10:16:42 -0800

    Implement removeFromParent() function and .parent property in tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  newTree.parent = null;
   // your code here
   extend(newTree, treeMethods);
   newTree.children = [];  // fix me
@@ -14,18 +14,30 @@ var treeMethods = {};
 treeMethods.addChild = function(value) {
   var child = Tree(value);
   this.children.push(child);
+  child.parent = this;
 };
 
 treeMethods.contains = function(target) {
   var result = false;
   if (this.value === target) {
     result = true;
-  } else if (this.children.length !== 0) {
+  } else if (this.children.length) {
     for (var i = 0; i < this.children.length; i++) {
       result = result || this.children[i].contains(target);
     }
   }
   return result;
+};
+
+treeMethods.removeFromParent = function() {
+  var parentChildren = this.parent.children; // an Array
+  for (var i = 0; i < parentChildren.length; i++) {
+    if (parentChildren[i] === this) {
+      parentChildren.splice(i, 1);
+    }
+  }
+  this.parent = null;
+  return this;
 };
 
 var extend = function(destination, obj) {
