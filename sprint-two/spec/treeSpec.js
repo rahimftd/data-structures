2f84c224364ab80534791ffff418978ce8f0f2c2
rahimftd <rahim.dharssi@gmail.com>
2016-03-05 15:36:05 -0800

    Create tests for tree traverse method

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,9 +5,10 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild", "contains", and "removeFromParent" and a property named "value" and a propetry named "parent"', function() {
+  it('should have methods named "addChild", "contains", "traverse" and "removeFromParent" and a property named "value" and a propetry named "parent"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
+    expect(tree.traverse).to.be.a('function');
     expect(tree.removeFromParent).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
     expect(tree.hasOwnProperty('parent')).to.equal(true);
@@ -65,6 +66,18 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  it('should execute callback on all nodes when traverse is called', function() {
+    var array = [];
+    tree.value = 1;
+    tree.addChild(2);
+    tree.addChild(3);
+    tree.children[0].addChild(4);
+    tree.traverse(function(node) {
+      array.push(node.value);
+    });
+    expect(array).to.deep.equal([1, 2, 4, 3]);
   });
 
 });
