65f849938e05076a34b276935d738ac72ffc6cb8
rahimftd <rahim.dharssi@gmail.com>
2016-03-05 10:16:01 -0800

    Create new tests for .pratent property and removeFromParent function

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,10 +5,20 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "contains", and "removeFromParent" and a property named "value" and a propetry named "parent"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
+    expect(tree.removeFromParent).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
+    expect(tree.hasOwnProperty('parent')).to.equal(true);
+  });
+
+  it('should have a parent property that points to the correct tree', function() {
+    tree.value = 1;
+    tree.addChild(2);
+    tree.children[0].addChild(3);
+    expect(tree.children[0].parent).to.equal(tree);
+    expect(tree.children[0].children[0].parent).to.equal(tree.children[0]);
   });
 
   it('should add children to the tree', function() {
@@ -24,6 +34,22 @@ describe('tree', function() {
   it('should return false for a value that was not added', function() {
     tree.addChild(5);
     expect(tree.contains(6)).to.equal(false);
+  });
+
+  it('should remove parent property when "removeFromParent" is called', function() {
+    tree.value = 1;
+    tree.addChild(2);
+    var child = tree.children[0];
+    expect(tree.children[0].parent).to.equal(tree);
+    tree.children[0].removeFromParent();
+    expect(child.parent).to.equal(null);
+  });
+
+  it('should return the child when "removeFromParent" is called', function() {
+    tree.value = 1;
+    tree.addChild(2);
+    var child = tree.children[0];
+    expect(tree.children[0].removeFromParent()).to.equal(child);
   });
 
   it('should be able to add children to a tree\'s child', function() {
