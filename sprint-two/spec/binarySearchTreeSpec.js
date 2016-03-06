a7d6d2ce0ba1c2f4a38ec3255642adea3278a420
rahimftd <rahim.dharssi@gmail.com>
2016-03-05 16:18:49 -0800

    Create tests for breadthFirstLog method

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -5,10 +5,11 @@ describe('binarySearchTree', function() {
     binarySearchTree = BinarySearchTree(5);
   });
 
-  it('should have methods named "insert", "contains", and "depthFirstLog', function() {
+  it('should have methods named "insert", "contains", "breadthFirstLog", and "depthFirstLog', function() {
     expect(binarySearchTree.insert).to.be.a('function');
     expect(binarySearchTree.contains).to.be.a('function');
     expect(binarySearchTree.depthFirstLog).to.be.a('function');
+    expect(binarySearchTree.breadthFirstLog).to.be.a('function');
   });
 
   it('should insert values at the correct location in the tree', function() {
@@ -35,5 +36,16 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(3);
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3]);
+  });
+
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(6);
+    binarySearchTree.breadthFirstLog(func, binarySearchTree);
+    expect(array).to.eql([5, 2, 6, 1, 3]);
   });
 });
