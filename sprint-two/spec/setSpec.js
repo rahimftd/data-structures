a206e283e671d74446b18c5ff1a0c3c30795c864
rahimftd <rahim.dharssi@gmail.com>
2016-03-05 16:39:01 -0800

    Add additional tests for handling non-strings in tree

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -24,4 +24,33 @@ describe('set', function() {
     expect(set.contains('Mel Gibson')).to.equal(false);
   });
 
+  it('should be able to add and remove numbers', function() {
+    set.add(1);
+    set.remove(1);
+    set.add(2);
+    expect(set.contains(1)).to.equal(false);
+    expect(set.contains(2)).to.equal(true);
+  });
+
+  it('should be able to add and remove booleans', function() {
+    set.add(true);
+    set.remove(true);
+    set.add(false);
+    expect(set.contains(true)).to.equal(false);
+    expect(set.contains(false)).to.equal(true);
+  });
+
+  it('should be able to add and remove objects and arrays', function() {
+    set.add({ one: 1 });
+    set.remove({ one: 1 });
+    set.add({ two: 2 });
+    set.add([1, 2, 3]);
+    set.remove([1, 2, 3]);
+    set.add([4, 5, 6]);
+    expect(set.contains({ one: 1 })).to.equal(false);
+    expect(set.contains({ two: 2 })).to.equal(true);
+    expect(set.contains([1, 2, 3])).to.equal(false);
+    expect(set.contains([4, 5, 6])).to.equal(true);
+  });
+
 });
